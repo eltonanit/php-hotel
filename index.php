18820261a5d73cbd06087bbb5291358abf09a93e
eltonanit <elton.ani@gmail.com>
2024-09-11 15:51:08 +0200

    [UPD] Fix html tags

--- index.php
+++ index.php
@@ -54,18 +54,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+     <meta name="description" content="PHP Hotel Listing Example">
     <title>PHP HOTEL </title>
 </head>
 <body>
    
  
-     <div class="container">
-        <div class="row gy-3">
-            <div class="col-12">
+<div class="container">
+    <div class="row gy-3">
+        <div class="col-12">
             <h2 class="text-center"> ESERCIZIO HOTEL </h2>
-          
-<table class="table">   
-  <thead>
+        </div>
+        <table class="table">
+            <thead>
     <tr>
       <th scope="col">#</th>
       <th scope="col">HOTEL</th>
@@ -91,6 +92,6 @@
 </table>
 </div>
 </div>
- 
+
 </body>
 </html>
